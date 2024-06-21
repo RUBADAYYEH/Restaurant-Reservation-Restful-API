40b101ad83495b0042e8f3c1c9439756403ed351
ruba dayy <ruba.dayyeh@gmail.com>
2024-06-21 21:41:55 +0300

    EmployeeController

---
+++ Profiles/EmployeeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservationDb.domain;
+using RetaurantReservationAPI.Models;
+
+namespace RetaurantReservationAPI.Profiles
+{
+    public class EmployeeProfile:Profile
+    {
+        public EmployeeProfile() {
+            CreateMap<Employee, Models.EmployeeDto>();
+            CreateMap<EmployeeDto, Employee>();
+  
+        }
+    }
+}
---
+++ Services/IReservationRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantReservationDb.domain;
+
+namespace RetaurantReservationAPI.Services
+{
+    public interface IReservationRepository
+    {
+        Task<IEnumerable<Reservation>> GetReservationsByCustomerIdAsync(int customerid);
+        Task<IEnumerable<Order>> GetOrdersByReservationIdAsync(int reservationId);
+        Task<IEnumerable<OrderItem>> GetOrderItemsByReservationIdAsync(int reservationId);
+
+    }
+}
